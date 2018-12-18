9d141ac686c8a75cf7fda1969ca4cd7e1a760203
Skywind <me@iskywind.com>
2018-12-18 23:35:54 +0800

    use static import

--- search_source/Nyaa.py
+++ search_source/Nyaa.py
@@ -3,7 +3,7 @@ from __future__ import print_function, unicode_literals
 
 import re, os
 import importlib
-Base = importlib.import_module('.base', __package__)
+import search_source.base as Base
 
 import requests
 from bgmi.utils import print_error
